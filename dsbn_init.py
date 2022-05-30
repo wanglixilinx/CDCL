f5ee6936629651daeb77f9773ca12c3b6d042df4
wanglixilinx <54088587+wanglixilinx@users.noreply.github.com>
2022-05-30 16:35:50 +0800

    Create dsbn_init.py

---
+++ dsbn_init.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+def _update_initial_weights_dsbn(state_dict, num_classes=1000, num_domains=2):
+    new_state_dict = state_dict.copy()
+
+    for key, val in state_dict.items():
+        update_dict = True
+
+        if (update_dict):
+            if 'weight' in key:
+                for d in range(num_domains):
+                    new_state_dict[key[0:-6] + 'bns.{}.weight'.format(d)] = val.data.clone()
+
+            elif 'bias' in key:
+                for d in range(num_domains):
+                    new_state_dict[key[0:-4] + 'bns.{}.bias'.format(d)] = val.data.clone()
+
+            if 'running_mean' in key:
+                for d in range(num_domains):
+                    new_state_dict[key[0:-12] + 'bns.{}.running_mean'.format(d)] = val.data.clone()
+
+            if 'running_var' in key:
+                for d in range(num_domains):
+                    new_state_dict[key[0:-11] + 'bns.{}.running_var'.format(d)] = val.data.clone()
+
+            if 'num_batches_tracked' in key:
+                for d in range(num_domains):
+                    new_state_dict[
+                        key[0:-len('num_batches_tracked')] + 'bns.{}.num_batches_tracked'.format(d)] = val.data.clone()
+
+    if num_classes != 1000 or len([key for key in new_state_dict.keys() if 'fc' in key]) > 1:
+        key_list = list(new_state_dict.keys())
+        for key in key_list:
+            if 'fc' in key:
+                print('pretrained {} are not used as initial params.'.format(key))
+                del new_state_dict[key]
+
+    return new_state_dict
